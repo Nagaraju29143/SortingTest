6e1b75a98d3d2df052cd59e093afa56d3692c770
Nagaraju29143 <nagaraju.revalla3@gmail.com>
2020-08-05 00:39:09 +0530

    sorting test changes

--- src/SortTest.java
+++ src/SortTest.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class SortTest {
 
 	public static void main(String[] args) {
 
-		Employee e1=new Employee("nag",30);
+		/*Employee e1=new Employee("nag",30);
 		Employee e2=new Employee("test",29);
 		Employee e3=new Employee("sai",25);
 		Employee e4=new Employee("ram",9);
@@ -20,7 +23,27 @@ public class SortTest {
 		
         Collections.sort(empList, Comparator.comparing(Employee::getAge).thenComparing(Employee::getName));
 
-        empList.stream().filter(e->e.getAge()>=10).map(Employee::getName).forEach(System.out::println);
+        empList.stream().filter(e->e.getAge()>=10).map(Employee::getName).forEach(System.out::println);*/
+        
+        Map<Character,String> hm=new HashMap<>();
+        hm.put('A',"Amazon");
+        hm.put('D',"Deloitee");
+        hm.put('B',"Boieng");
+        hm.put('F',"Flipcart");
+		
+		Character[] input= {'D','A','B'};
+	    List<String> list=new ArrayList<>();
+		 
+	    Arrays.stream(input).forEach(c->{
+	    String  str= hm.entrySet().stream()
+			    .filter(e->e.getKey().equals(c))
+			    .map(Map.Entry::getValue)
+			    .collect(Collectors.joining(","));
+			
+			     list.add(str);
+	    });
+		
+		list.stream().sorted(Comparator.comparing(String::toString)).forEach(System.out::println);
         
 	}
 
